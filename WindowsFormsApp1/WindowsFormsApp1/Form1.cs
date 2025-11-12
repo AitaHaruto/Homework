6faaa7acabf7de02142c58a96b63f5ef35f7e238
AitaHaruto <mazirenja0212@gmail.com>
2025-11-12 10:34:25 +0900

    数字のボタンだけ作った

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -11,10 +11,25 @@ using System.Windows.Forms;
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
-    {
+    { 
         public Form1()
+            
         {
+            int count = 1;
             InitializeComponent();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {  
+                    NumberButton numberbutton = new NumberButton(this,new Size(100,100),new Point(j*100,i*100), $"{count}",i,j);
+
+                    Controls.Add(numberbutton);
+                    count++;
+                }
+            }
+           //"0"用.
+            NumberButton zero = new NumberButton(this, new Size(100, 100), new Point(100, 300), $"{0}", 3, 1);
+            Controls.Add(zero);
         }
     }
 }
---
+++ WindowsFormsApp1/WindowsFormsApp1/NumberButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class NumberButton:Button
+    {
+      
+        
+        
+
+        private int _y;
+        
+        private int _x;
+        private Form1 _form1;
+        public  NumberButton(Form1 form1, Size size, Point location, string text,int x, int y)
+        {
+            _form1 = form1;
+             Size = size;
+            Location = location;
+            Text = text;
+            _x = x;
+            _y = y;
+            Click += NumberButton_Click;
+
+
+        }
+
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            int num1;
+            int num2;
+
+        }
+    }
+}
