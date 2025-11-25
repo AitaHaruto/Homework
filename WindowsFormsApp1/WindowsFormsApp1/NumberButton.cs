524e4159f7eac05e926a3b5c42124ddcbe9910f6
AitaHaruto <mazirenja0212@gmail.com>
2025-11-25 12:40:15 +0900

    いちいちぼたんを消したり再生成する必要がないことに気づき、ツールボックスからボタンを配置、すると一気にらくにかけるようになったので、あとは計算処理のみ。

--- WindowsFormsApp1/WindowsFormsApp1/NumberButton.cs
+++ WindowsFormsApp1/WindowsFormsApp1/NumberButton.cs
@@ -10,44 +10,7 @@ using System.Windows.Forms;
 namespace WindowsFormsApp1
 {
     public class NumberButton:Button
-    { 
-
-        private int _y;
-        
-        private int _x;
-
-        private int _z;
-
-        private Form1 _form1;
-
-        private OperatorButton _operatorbutton;
-        public  NumberButton(Form1 form1, Size size, Point location, string text,int x, int y,int z)
-        {
-            _form1 = form1;
-             Size = size;
-            Location = location;
-            Text = text;
-            _x = x;
-            _y = y;
-            _z = z;
-            Click += NumberButton_Click;
-
-
-        }
-
-        public void NumberButton_Click(object sender, EventArgs e)
-        {
-            int? num1 = null;
-            int? num2 = null;
-            if (num1 == null)
-            {
-                num1 = _form1._numberArray[_x, _y]._z;
-            }
-            else 
-            {
-                num2 = _form1._numberArray[_x, _y]._z;
-            }
-            
-        }
+    {
+      
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/OperatorButton.cs
+++ WindowsFormsApp1/WindowsFormsApp1/OperatorButton.cs
@@ -9,68 +9,8 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public class OperatorButton:Button
+    public class OperatorButton : Button
     {
-        int state = 0;
-        private int _x;
-        private int _y;
-        private Form1 _form1;
-        private NumberButton _numberbutton;
-        public OperatorButton(Form1 form1, Size size, Point location, string text, int x)
-        {
-            _form1 = form1;
-            Size = size;
-            Location = location;
-            Text = text;
-            _x = x;
-            
-            Click += Operator_Click;
-        }
-        public void Addtion()
-        {
-            if (_form1._operatorArray[_x].Text == "+")
-            {
-                state = 1;
-            }
-        }
-        public void Subtraction()
-        {
-            if (_form1._operatorArray[_x].Text == "-")
-            {
-                state = 2;
 
-            }
-        }
-        public void Multiplication()
-        {
-            if (_form1._operatorArray[_x].Text == "×")
-            {
-                state = 3;
-            }
-
-        }
-        public void Division()
-        {
-            if (_form1._operatorArray[_x].Text == "÷")
-            {
-                state = 4;
-            }
-
-        }
-        public void Equal()
-        {
-            if (_form1._operatorArray[_x].Text == "＝")
-            {
-                if (state == 1)
-                { 
-                
-                }
-            }
-        }
-        public void Operator_Click(object sender, EventArgs e)
-        {
-
-            
-        }
     }
 }
